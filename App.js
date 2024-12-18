8c049b4b55dcd4cee3cbcf206f974446ed0ea07d
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-12-18 13:46:20 +0530

    Add attributes using react and give styles using css

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 // create element using React
-const heading = React.createElement("h1", {}, "Hello World from React!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
 
 // create root where we put that element using React-DON
 const root = ReactDOM.createRoot(document.getElementById("root"));
