dae6d8fb76e2cc16725590518cbc4a59e5dbed2f
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-12-18 13:15:21 +0530

    Add external js file App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+// create element using React
+const heading = React.createElement("h1", {}, "Hello World from React!");
+
+// create root where we put that element using React-DON
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//render the element
+root.render(heading);
