005bbffdb2647ea8c87b23e6c4d3d826456dfb3f
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-12-18 14:04:40 +0530

    Create Nested Elements using React

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-// create element using React
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+//create nested elements using react
+/*
+<div id="parent">
+   <div id="child">
+      <h1>I am a Nested Element</h1>
+    </div>
+</div>
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am a Nested Element")
+  )
 );
 
 // create root where we put that element using React-DON
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //render the element
-root.render(heading);
+console.log(parent);
+
+root.render(parent);
