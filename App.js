4506404d9e7f56d7c3b9194c1d062b8b641f105a
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-12-20 12:54:40 +0530

    Create nested elements with childs and each child has siblings using react

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-//create nested elements with siblings using react
+//create nested elements with childs and each child has siblings using react
 /*
 <div id="parent">
-   <div id="child">
+   <div id="child1">
+      <h1>I am a Nested Element</h1>
+      <h2>I am a Nested Element</h2>
+    </div>
+   <div id="child2">
       <h1>I am a Nested Element</h1>
       <h2>I am a Nested Element</h2>
     </div>
 </div>
 */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag of child1 "),
+    React.createElement("h2", {}, "I am h2 tag of child1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag child2"),
+    React.createElement("h2", {}, "I am h2 tag child2"),
+  ]),
+]);
 
 // create root where we put that element using React-DON
 const root = ReactDOM.createRoot(document.getElementById("root"));
