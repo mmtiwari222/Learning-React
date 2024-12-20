7b610c6becd6ec31f5602f3e9d3b125e777115d6
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-12-20 12:48:10 +0530

    Create nested element with siblings

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-//create nested elements using react
+//create nested elements with siblings using react
 /*
 <div id="parent">
    <div id="child">
       <h1>I am a Nested Element</h1>
+      <h2>I am a Nested Element</h2>
     </div>
 </div>
 */
@@ -10,11 +11,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am a Nested Element")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
 );
 
 // create root where we put that element using React-DON
